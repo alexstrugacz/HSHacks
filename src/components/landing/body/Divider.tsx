bd7537e6d70e6fbf4af67d7107acd90b481ecb34
Alex Strugacz <90818590+Nectar0@users.noreply.github.com>
2023-01-15 19:49:32 -0600

    divider gradient

--- src/components/landing/body/Divider.tsx
+++ src/components/landing/body/Divider.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 const Divider: React.FC<{}> = (props) => {
     return (
-        <div className={"bg-gradient-to-r py-5 flex justify-center from-[#FF5360] to-[#21E4FC]"}>
+        <div className={"bg-gradient-to-t py-5 flex justify-center from-[#FF6200] to-[#FDA766]"}>
             <p className={"text-white font-Poppins font-bold text-lg md:text-2xl"}>What we'll do at Innovate Chicago</p>
         </div>
     );
