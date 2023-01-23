29110704c59ecd6943299b133e9f889ef773a12a
Alex Strugacz <90818590+Nectar0@users.noreply.github.com>
2023-01-23 11:58:14 -0600

    sponsor improvements

--- src/components/sponsors/Prospectus.tsx
+++ src/components/sponsors/Prospectus.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 const sponsorPackages = require('../../assets/images/ICSponsorPackages.png')
-const ProspectusPDF = require('../../assets/Innovate_Chicago_Prospectus.pdf')
+const ProspectusPDF = require('../../assets/Innovate_Chicago_Sponsorsv2.pdf')
 
 
 const Prospectus: React.FC<{}> = () => {
@@ -8,20 +8,20 @@ const Prospectus: React.FC<{}> = () => {
         <div>
             <h1 className='text-neutral-700 font-Poppins font-bold text-4xl'>Our Prospectus</h1>
             <div className='mt-8'>
-                <a className='hover:ring-offset-1 hover:ring-4 transition-all text-black ring-orange-300 font-Poppins font-bold bg-orange-300 rounded px-8 py-2' href={ProspectusPDF} download="Prospectus">Download</a>
+                <a className='hover:ring-offset-1 hover:ring-4 transition-all text-black ring-orange-300 font-Poppins font-bold bg-orange-300 rounded px-8 py-2' href={ProspectusPDF} download="Prospectus">Download as PDF</a>
             </div>
-            <h1 className='text-orange-400 font-Poppins font-bold text-3xl mt-12'><a className='text-black'>What is </a>Innovate Chicago?</h1>
+            <h1 className='text-red-400 font-Poppins font-bold text-3xl mt-12'><a className=''>Empowering </a>Future Developers</h1>
             <div className='text-neutral-500 text-xl mt-3'>
                 <p className=''>Innovate Chicago is a 12-hour high school hackathon organized by the <a href='https://herseyhack.club' className='hover:font-bold transition-all underline'>JHHS Hack Club</a>. It will be held at <a href='https://www.google.com/maps/place/John+Hersey+High+School/@42.1038234,-87.9612505,17z/data=!3m1!4b1!4m5!3m4!1s0x880fbbad9a8cbbcd:0xbc3eb41efde43b5f!8m2!3d42.1038234!4d-87.9590565' className='hover:font-bold transition-all underline'>John Hersey High School</a> in Arlington Heights, IL. from 8AM to 8PM on April 15, 2023.</p>
                 <br></br>
                 <p>In a day full of collaboration and creativity, 90 attendees (projected) will bring their ideas to reality, learning skills far beyond just tech. With help from experienced professionals and mentors, students will learn invaluable skills by collaboratively building projects, joining engaging workshops, and more!</p>
             </div>
             <div>
-                <h1 className='text-orange-400 font-Poppins font-bold text-3xl mt-16'><a className='text-black'>Where you </a>come in</h1>
+                <h1 className='text-red-400 font-Poppins font-bold text-3xl mt-16'><a className=''>Where you </a>come in</h1>
                 <p className='text-neutral-500 text-xl mt-3'>Your sponsorship is critical to Innovate Chicago providing this life-changing experience. We're seeking sponsorships to buy food, prizes, and other supplies to run the event. By sponsoring Innovate Chicago, you'll directly contribute to the personal growth of 90+ local high schoolers.</p>
             </div>
             <div>
-                <h1 className='text-orange-400 font-Poppins font-bold text-3xl mt-20'><a className='text-black'>Benefits of </a>sponsorship</h1>
+                <h1 className='text-red-400 font-Poppins font-bold text-3xl mt-20'><a className=''>Benefits of </a>sponsorship</h1>
                 <div className='flex mt-8 align-center'>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-7 h-7">
                         <path fillRule="evenodd" d="M2.25 2.25a.75.75 0 000 1.5H3v10.5a3 3 0 003 3h1.21l-1.172 3.513a.75.75 0 001.424.474l.329-.987h8.418l.33.987a.75.75 0 001.422-.474l-1.17-3.513H18a3 3 0 003-3V3.75h.75a.75.75 0 000-1.5H2.25zm6.54 15h6.42l.5 1.5H8.29l.5-1.5zm8.085-8.995a.75.75 0 10-.75-1.299 12.81 12.81 0 00-3.558 3.05L11.03 8.47a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 001.146-.102 11.312 11.312 0 013.612-3.321z" clipRule="evenodd" />
@@ -57,9 +57,13 @@ const Prospectus: React.FC<{}> = () => {
                 <p className='text-neutral-500 text-xl mt-3'>Inventive minds and fresh eyes can help bring a new mindset to a challenge or problem. Let Innovate Chicago participants take a crack at challenging problems in your field of operations or in the larger world.</p>
             </div>
             <div>
-                <h1 className='text-orange-400 font-Poppins font-bold text-3xl mt-20'><a className='text-black'>Sponsor </a>packages</h1>
-                <p className='mt-3 text-lg text-neutral-600'>Contact <a href='mailto:sponsors@innovatechicago.net' className='hover:underline font-bold text-green-500'>sponsors@innovatechicago.net</a> for next steps or further inquires.</p>
+                <h1 className='text-red-400 font-Poppins font-bold text-3xl mt-20'><a className=''>Sponsor </a>packages</h1>
+                <p className='mt-3 text-xl mb-12 text-neutral-600'>If you have any questions or would like to discuss adjustments for packages, please contact <a href='mailto:sponsors@innovatechicago.net' className='hover:underline font-bold text-black'>sponsors@innovatechicago.net</a>.</p>
                 <img className="mt-10" src={sponsorPackages}></img>
+                <div className='text-center mt-12 mb-24'>
+                    <p className='font-Poppins text-lg'>Ready to inspire the next generation of developers?</p>
+                    <p className='font-Poppins text-xl'>Contact <a href='mailto:sponsors@innovatechicago.net' className='font-bold'>sponsors@innovatechicago.net</a> to discuss next steps!</p>
+                </div>
             </div>
         </div>
     )
