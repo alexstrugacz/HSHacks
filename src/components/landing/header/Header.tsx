ef9182a9584dbe33863012e934c2be1e36f73dcf
nexus1010 <bot.developer3@gmail.com>
2023-02-27 18:30:18 -0600

    updated the landing page

--- src/components/landing/header/Header.tsx
+++ src/components/landing/header/Header.tsx
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 import { REGISTER_LINK } from "../../../global/links";
 import RegisterButton from "../../../global/RegisterButton";
 import JoinTheDiscord from "../../../global/JoinTheDiscord";
+import { CalendarIcon, WrenchIcon } from "@heroicons/react/24/solid";
 
 const Header: React.FC<{}> = () => {
     const openHerseyLink = () => {
@@ -43,31 +44,29 @@ const Header: React.FC<{}> = () => {
                                 initial={{ opacity: 0, x: -100, scale: 0.8 }}
                                 animate={{ opacity: 1, x: 0, scale: 1 }}
                                 transition={{ delay: 0.2, duration: 0.6 }}
-                                className={"text-[#5BC0DE] text-5xl md:text-7xl font-Poppins font-bold"}>
+                                className={"text-[#5BC0DE] text-5xl md:text-6xl font-Poppins font-bold"}>
                                 Welcome to
                             </motion.h4>
                             <motion.h1
                                 initial={{ opacity: 0, x: -100, scale: 0.8 }}
                                 animate={{ opacity: 1, x: 0, scale: 1 }}
                                 transition={{ delay: 0.9, duration: 0.6 }}
-                                className="text-white font-bold text-7xl md:text-9xl font-Poppins mt-0">HSHacks.
+                                className="text-white font-bold text-5xl md:text-6xl font-Poppins mt-0">HSHacks.
                             </motion.h1>
                             <motion.div
                                 initial={{ opacity: 0, x: -100, scale: 0.8 }}
                                 animate={{ opacity: 1, x: 0, scale: 1 }}
                                 transition={{ delay: 1.4, duration: 0.6 }}
                             >
-                                <p className="text-slate-300 text-lg md:text-xl pr-5 font-Poppins pt-8 max-w-xl">Build projects for a nonprofit, make new friends, and have a blast at the <a className={"text-white font-bold"}>HSHacks Hackathon!</a> <a className={"text-zinc-400"}>Organized by the <a href={"https://herseyhack.club"} target={"_blank"} className={"transition-all hover:cursor-pointer hover:text-white"}>JHHS Hack Club</a>.</a></p>
-                                <div className="mt-8">
-                                    <div onClick={openHerseyLink} className="group flex gap-2 mr-16 items-center hover:cursor-pointer w-fit">
-                                        <MapPinIcon className={"w-7 text-white group-hover:text-zinc-400 transition-colors"} />
-                                        <h2 className="font-bold text-white font-Poppins group-hover:text-zinc-400 transition-colors text-xl mb-0">John Hersey High School ARC</h2>
+                                <p className="text-slate-300 text-md md:text-xl pr-5 font-Poppins pt-2 md:pt-8 max-w-xl">Build projects for a nonprofit, make new friends, and have a blast at the <a className={"text-white font-bold"}>HSHacks Hackathon!</a></p>
+                                <div className="mt-5 md:mt-8">
+                                    <div onClick={openHerseyLink} className="group flex gap-1 mr-16 items-center hover:cursor-pointer w-fit">
+                                        <MapPinIcon className={"w-6 text-white group-hover:text-zinc-400 transition-colors"} />
+                                        <h2 className="font-bold text-white font-Poppins group-hover:text-zinc-400 transition-colors text-md md:text-xl mb-0">John Hersey High School ARC</h2>
                                     </div>
-                                    <div className="flex mt-3">
-                                        <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
-                                            <path d="M7.47028 3V5.25M17.9703 3V5.25M3.72028 18.75V7.5C3.72028 6.90326 3.95733 6.33097 4.37929 5.90901C4.80124 5.48705 5.37354 5.25 5.97028 5.25H19.4703C20.067 5.25 20.6393 5.48705 21.0613 5.90901C21.4832 6.33097 21.7203 6.90326 21.7203 7.5V18.75M3.72028 18.75C3.72028 19.3467 3.95733 19.919 4.37929 20.341C4.80124 20.7629 5.37354 21 5.97028 21H19.4703C20.067 21 20.6393 20.7629 21.0613 20.341C21.4832 19.919 21.7203 19.3467 21.7203 18.75M3.72028 18.75V11.25C3.72028 10.6533 3.95733 10.081 4.37929 9.65901C4.80124 9.23705 5.37354 9 5.97028 9H19.4703C20.067 9 20.6393 9.23705 21.0613 9.65901C21.4832 10.081 21.7203 10.6533 21.7203 11.25V18.75" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                        </svg>
-                                        <h2 className="font-bold text-white font-Poppins text-xl">April 15, 2023</h2>
+                                    <div className="flex mt-2 md:mt-3 gap-1">
+                                        <CalendarIcon className={"w-6 text-white"} />
+                                        <h2 className="font-bold text-white font-Poppins text-md md:text-xl">April 15, 2023</h2>
                                     </div>
                                 </div>
                                 <JoinTheDiscord />
--- src/global/JoinTheDiscord.tsx
+++ src/global/JoinTheDiscord.tsx
@@ -8,7 +8,7 @@ const JoinTheDiscord: React.FC<{}> = (props) => {
     return (
         <div>
             <a href={JOIN_THE_DISCORD} target="_blank">
-                <button className={`text-white font-Poppins text-xl font-bold py-3 px-5 pr-3 mt-12 rounded-full bg-gradient-to-tl  from-[#CF424D] to-[#E96C2E] hover:scale-105 transition-all`}>
+                <button className={`text-white font-Poppins text-lg md:text-xl font-bold py-3 px-5 pr-3 mt-12 rounded-full bg-gradient-to-tl  from-[#CF424D] to-[#E96C2E] hover:scale-105 transition-all`}>
                     <div className={"flex items-center gap-5"}>
                         <p>Join the Discord</p>
                         <ChevronRightIcon className={"text-white w-5 h-5 stroke-1 stroke-white"} />
