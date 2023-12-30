79bbffdded5bdebde50f4b6b1ad78e1cbec07766
KeenanOH <86394469+KeenanOH@users.noreply.github.com>
2023-12-30 13:55:32 -0600

    Added a semicolon in Landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import Header from "./landing/header/Header";
 import Body from "./landing/body/Body";
 import Footer from "./landing/body/footer/Footer";
